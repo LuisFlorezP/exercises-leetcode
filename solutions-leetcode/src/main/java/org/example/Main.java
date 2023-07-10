fe737465b6b4b55a0b5bb1b4228c2fc95764adf8
Luis Florez <braian1k3@gmail.com>
2023-07-10 14:32:24 -0500

    Finish exercise 1 level easy.

---
+++ solutions-leetcode/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.example.easy.ArrayPartition;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // ArrayPartition ---------->
+        /*
+            int[] array = new int[4];
+            array[0] = 1;
+            array[1] = 4;
+            array[2] = 3;
+            array[3] = 2;
+            System.out.println("Resultado: " + ArrayPartition.arrayPairSum(array));
+        */
+
+    }
+}
